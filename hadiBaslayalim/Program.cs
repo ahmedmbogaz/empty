142bdc04f0f2eafbfd36835289d2ef20f556b2cf
ahmedmbogaz <77534733+ahmedmbogaz@users.noreply.github.com>
2021-01-17 00:44:43 +0300

    Add files via upload

---
+++ hadiBaslayalim/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hadiBaslayalim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //do not repeat yourself = kendini tekrarlama
+
+            string kategoriEtiketi = "kategori";
+            int ogrencisayisi = 32000;
+            double FaizOrani = 1.46;
+            bool SistemeGirisYapmismi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi");
+            }
+
+
+            if (SistemeGirisYapmismi==true)
+            {
+                Console.WriteLine("Ayarlar");
+            }
+            else
+            {
+                Console.WriteLine("kategori");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
